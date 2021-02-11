6a704a756affc04cabbb8d25f016436bef9c186a
mohamad mirani <mohammadmirani@gmail.com>
2021-02-11 11:07:03 +0330

    initialize

---
+++ app.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const app = express();
+const path = require('path')
+
+app.set('views',path.join(__dirname,'views'))
+
+app.get('/',(req,res)=>{
+res.sendFile(path.join(__dirname,"views/index.html"))
+})
+
+
+
+
+
+app.listen(5005)
