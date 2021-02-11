8a2d19127d1d7b9be78110d258f05bd2d280226e
mohamad mirani <mohammadmirani@gmail.com>
2021-02-11 15:08:09 +0330

    edit1

--- app.js
+++ app.js
@@ -1,7 +1,8 @@
 const express = require('express');
 const app = express();
 const path = require('path')
-const fs = require('fs')
+const fs = require('fs');
+const { json } = require('express');
 
 app.set('views', path.join(__dirname, 'views'))
 app.set('view engine', 'ejs')
@@ -14,29 +15,45 @@ app.get('/', (req, res) => {
 
 
 
-    app.get('/ejs', (req, res) => {
-        fs.readFile(path.join(__dirname,"public/data.json"),function(err,data){
-            if(err) return res.status(400).send("Something went Wrong!!")
-            console.log(data);
+// app.get('/ejs', (req, res) => {
+//     fs.readFile(path.join(__dirname, "public/data.json"), function (err, data) {
+//         if (err) return res.status(400).send("Something went Wrong!!")
+//         console.log(data);
 
-            res.render('index.ejs', 
-            JSON.parse(data)
-            // {
-            //     firstName: "Mohammad",
-            //     lastName: "Mirani",
-            //     skills:['js', 'html','css']
-            // }
-            )
-        })
+//         res.render('index.ejs',{
+//                 firstName: "Mohammad",
+//                 lastName: "Mirani",
+//                 skills: ['js', 'html', 'css']
+//             }
+//         )
+//     })
+// })
 
-    
+// app.get('/ejs',(req,res)=>{
+//     fs.readFile(path.join(__dirname,"public/data.json",'utf8',function(err,data){
+//         if(err) return res.status(400).send("Something went wrong!!!")
+//         // console.log(data);
+//         res.render('index.ejs',JSON.parse(data))
+//     }))
+// })
+
+app.get('/ejs', (req, res) => {
+
+    fs.readFile(path.join(__dirname, "public/data.json"), "utf8", function(err, data) {
+        if (err) return res.status(400).send("Something went wrong!!!")
+        
+        res.render('index.ejs', JSON.parse(data))
     })
 
-app.get('/home',(req,res)=>{
-        res.render('pages/home')
+});
+
+
+
+app.get('/home', (req, res) => {
+    res.render('pages/home')
 })
 
-app.get('/about',(req,res)=>{
+app.get('/about', (req, res) => {
     res.render('pages/about')
 })
 
